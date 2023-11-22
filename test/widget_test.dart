cfe4bba8f552fba149955c6bab612264ac65fbb4
EugeneXu <diakenxu@gmail.com>
2023-11-21 19:42:15 -0500

    base

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:swipe/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Swipe());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
